f84105d60316f4d565c113c5a77536e29f82c02b
nonon1234 <funnytoybox94@gmail.com>
2020-10-11 20:05:44 +0900

    名前入力とパスワードのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
+  validates :nickname, :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,message: 'Full-width katakana characters' } do
+    validates :last_name, :first_name, :last_name_kana, :first_name_kana
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+
 end
