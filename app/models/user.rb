b17a19d18b63bccce6e39f5b757c3e384b68e201
nonon1234 <funnytoybox94@gmail.com>
2020-10-13 23:11:38 +0900

    バリデーションの修正　全角カタカナを分岐

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,12 @@ class User < ApplicationRecord
 
   validates :nickname, :birthday, presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width katakana characters' } do
-    validates :last_name, :first_name, :last_name_kana, :first_name_kana
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+    validates :last_name, :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン一]+\z/, message: 'Full-width katakana characters' } do
+    validates :last_name_kana, :first_name_kana
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
