ffe65d67ea59df13c3df36909ac0d2eaf0f56c8c
nonon1234 <funnytoybox94@gmail.com>
2020-10-13 23:12:30 +0900

    単体テストコードの作成　FactoryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,13 +3,13 @@ FactoryBot.define do
     Faker::Config.locale = 'ja'
     nickname { Faker::Name.last_name }
     email { Faker::Internet.free_email }
-    password = Faker::Internet.password(min_length: 6)
+    password = Faker::Internet.password(min_length: 10)
     password { password }
     password_confirmation { password }
-    last_name { Faker::Name.last_name }
-    first_name { Faker::Name.first_name }
-    last_name_kana { Faker::Name.last_name }
-    first_name_kana { Faker::Name.first_name }
+    last_name { Faker::Name.first_name }
+    first_name { Faker::Name.last_name }
+    last_name_kana { 'イロハ' }
+    first_name_kana { 'アイウエオ' }
     birthday { Faker::Date.between(from: '1930-01-01', to: '2015-12-31') }
   end
 end
