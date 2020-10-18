fa0023ac77e954e3bd5a64b059159a7a13beb4a4
nonon1234 <funnytoybox94@gmail.com>
2020-10-18 15:35:49 +0900

    ActiveHash　カテゴリーの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
   has_one_attached :image
+
+  validates :category, presence: true
+
+  validates :category_id, numericality: { other_than: 0 }
 end
