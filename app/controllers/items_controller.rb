499e16ad5ca5931c0ba3ea9ae143b96e275c45d1
nonon1234 <funnytoybox94@gmail.com>
2020-10-25 21:55:12 +0900

    商品登録機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,35 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, 
+      :description, 
+      :category_id, 
+      :status_id, 
+      :sipping_fee_id, 
+      :sipping_area_id, 
+      :sipping_time_id, 
+      :price
+    ).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :prefecture, :status, :sipping_fee, :sipping_time
+
+  berongs_to :user
   has_one_attached :image
 
-  validates :category, :prefecture, :status, :sipping_fee, :sipping_time, presence: true
-
+  validates :name, :description, :category, :prefecture, :status, :sipping_fee, :sipping_time, :price, presence: true
   validates :category_id, :prefecture_id, :status_id, :sipping_fee_id, :sipping_time_id, numericality: { other_than: 0 }
 end
