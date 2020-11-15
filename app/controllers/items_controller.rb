255ec8f00546e7b6265c70b1554130b7a20a2a72
nonon1234 <funnytoybox94@gmail.com>
2020-11-15 21:32:01 +0900

    updateアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
   end
 
   private
