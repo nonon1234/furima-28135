f74c4acc6c44f18b8a64b1e07cac670960672731
nonon1234 <funnytoybox94@gmail.com>
2020-11-22 16:33:37 +0900

    商品情報を取り出しビューへ表示／出品ユーザーはトップページへリダイレクト

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,6 +2,8 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id
   end
 
   def create
