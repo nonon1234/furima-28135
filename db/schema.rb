b09352d59da28759b1d06e801e7ae763d70eeaf8
nonon1234 <funnytoybox94@gmail.com>
2020-12-12 19:47:31 +0900

    itemsテーブルに購入済み判定カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_120237) do
+ActiveRecord::Schema.define(version: 2020_12_12_103538) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_11_22_120237) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "sold", default: false, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
