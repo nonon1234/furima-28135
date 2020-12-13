2d82621e64529c24d0d58a88faac58649ec2db2b
nonon1234 <funnytoybox94@gmail.com>
2020-12-13 20:34:52 +0900

    バリデーションをまとめ、user_id,item_idを追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -3,12 +3,11 @@ class PurchaseAddress
   attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :token, :user_id, :item_id
 
   with_options presence: true do
+    validates :city, :block, :token, :user_id, :item_id
     validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input half-width characters.' }
   end
-  with_options presence: true do
-    validates :city, :block, :token
-  end
+  
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :phone_number, length: { maximum: 11 }
 
